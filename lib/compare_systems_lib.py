5f154541522ea47a8d012373d8f9391d610ef1de
Clark Cucinell <clarkc@holly.mcs.anl.gov>
2022-06-27 11:19:20 -0500

    adding inplace to set_index

--- lib/compare_systems_lib.py
+++ lib/compare_systems_lib.py
@@ -361,9 +361,7 @@ def run_pathways(genome_ids, query_dict, output_file,output_dir, genome_data, se
         ec_table['gene_count'] = [0]*ec_table.shape[0]
         ec_table['ec_count'] = [0]*ec_table.shape[0]
         ec_table['genome_ec'] = [0]*ec_table.shape[0]
-        import pdb  
-        pdb.set_trace()
-        genome_df.set_index('ec_index')
+        genome_df.set_index('ec_index',inplace=True)
         for ec_number in ec_table['ec_number']:
             tmp_df = genome_df.loc[ec_number]
             if isinstance(tmp_df, pd.DataFrame):
