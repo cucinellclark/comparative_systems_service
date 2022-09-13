fa23e8fb30f22c81f0e2b7c32a0ad151c752b073
Clark Cucinell <ac.cucinell@holly.mcs.anl.gov>
2022-09-13 16:45:49 -0500

    setting trace

--- lib/compare_systems_lib.py
+++ lib/compare_systems_lib.py
@@ -637,6 +637,10 @@ def run_pathways_v2(genome_ids, query_dict, output_file, output_dir, genome_data
     # get gene data frame 
     gene_df = query_dict['feature']
     genes_output = pd.merge(gene_df.drop(return_columns_to_remove('pathways_genes',gene_df.columns.tolist()), axis=1),pathway_df,on=['genome_id','patric_id'],how='inner')
+    import pdb
+    pdb.set_trace()
+    #genes_output.set_index(
+    #genes_output['genome_count'] = 
 
     pathway_output = '\n'.join(pathway_line_list)
     ec_output = '\n'.join(ec_line_list)
