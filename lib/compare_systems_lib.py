1e31ef331c1c0e1be7122bcc5181ba0a5afc74b8
Clark Cucinell <ac.cucinell@holly.mcs.anl.gov>
2022-09-24 10:22:05 -0500

    fixing bug

--- lib/compare_systems_lib.py
+++ lib/compare_systems_lib.py
@@ -650,13 +650,13 @@ def run_pathways_v2(genome_ids, query_dict, output_file, output_dir, genome_data
     genes_output = pd.merge(gene_df.drop(return_columns_to_remove('pathways_genes',gene_df.columns.tolist()), axis=1),pathway_df,on=['genome_id','patric_id'],how='inner')
 
     for idx in range(0,genes_output.shape[0]):
+        pathway_id = genes_output.iloc[idx].pathway_id
+        if pathway_id not in unique_pathway_features:
+            unique_pathway_features[pathway_id] = {}    
         gene = genes_output.iloc[idx].gene
         if gene is None or gene is np.nan:
             continue
-        pathway_id = genes_output.iloc[idx].pathway_id
         genome_id = genes_output.iloc[idx].genome_id
-        if pathway_id not in unique_pathway_features:
-            unique_pathway_features[pathway_id] = {}    
         if gene not in unique_pathway_features[pathway_id]:
             unique_pathway_features[pathway_id][gene] = set()
         unique_pathway_features[pathway_id][gene].add(genome_id)
