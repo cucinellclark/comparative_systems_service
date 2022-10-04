5bb927da47751d8a7caa5cdbb701032aee2ab951
Clark Cucinell <ac.cucinell@holly.mcs.anl.gov>
2022-10-04 10:02:14 -0500

    adding traces

--- lib/compare_systems_lib.py
+++ lib/compare_systems_lib.py
@@ -580,11 +580,15 @@ def run_pathways_v2(genome_ids, query_dict, output_file, output_dir, genome_data
             unique_pathways.add(pathway_id)
             unique_ecs.add(ec_number)
             #unique_features.add(patric_id)
-            #if pathway_id not in unique_pathway_features:
-            #    unique_pathway_features[pathway_id] = {} 
-            #if patric_id not in unique_pathway_features[pathway_id]: 
-            #    unique_pathway_features[pathway_id][patric_id] = set()
-            #unique_pathway_features[pathway_id][patric_id].add(genome_id)
+            '''
+            if pathway_id not in unique_pathway_features:
+                unique_pathway_features[pathway_id] = {} 
+            if 'gene' in pathway_fields:
+                pathway_gene = pathway_fields['gene']
+                if pathway_gene not in unique_pathway_features[pathway_id]: 
+                    unique_pathway_features[pathway_id][pathway_gene] = set()
+                unique_pathway_features[pathway_id][pathway_gene].add(genome_id)
+            '''
             if pathway_id not in unique_pathway_ecs:
                 unique_pathway_ecs[pathway_id] = {}
             if ec_number not in unique_pathway_ecs[pathway_id]:
@@ -648,9 +652,14 @@ def run_pathways_v2(genome_ids, query_dict, output_file, output_dir, genome_data
 
     genes_output = pd.merge(gene_df.drop(return_columns_to_remove('pathways_genes',gene_df.columns.tolist()), axis=1),pathway_df,on=['genome_id','patric_id'],how='inner')
 
+    import pdb
+    pdb.set_trace()
+
     if 'gene_x' in genes_output.columns:
         genes_output['gene'] = genes_output['gene_x']
         genes_output.drop(['gene_x','gene_y'],inplace=True,axis=1)
+
+    pdb.set_trace()
 
     for idx in range(0,genes_output.shape[0]):
         pathway_id = genes_output.iloc[idx].pathway_id
