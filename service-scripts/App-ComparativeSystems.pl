5270eb3ab6066bc30d32442de36c3335b6af1b46
Clark Cucinell <ac.cucinell@holly.mcs.anl.gov>
2022-10-18 10:13:15 -0500

    testing

--- service-scripts/App-ComparativeSystems.pl
+++ service-scripts/App-ComparativeSystems.pl
@@ -48,10 +48,9 @@ sub process_compsystems
     # TODO: Remove
     # testing and settuping up preflight check sdtuff
     my $api = P3DataAPI->new();
-    my @groups = $params->{genome_groups}; 
-    print scalar @groups . "\n";
+    my $groups = $params->{genome_groups}; 
     my $numGenomes = 0;
-    for my $gg (@groups) 
+    for my $gg (@$groups) 
     {
         print "@$gg\n";
         my $genomes = $api->retrieve_patric_ids_from_genome_group(@$gg);   
