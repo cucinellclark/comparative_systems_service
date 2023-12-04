468dec661167932876b3a2159f75296d2d6c368b
Clark Cucinell <ac.cucinell@holly.mcs.anl.gov>
2023-12-04 13:15:33 -0600

    adding check

--- lib/compare_systems_lib.py
+++ lib/compare_systems_lib.py
@@ -723,12 +723,10 @@ def run_pathways(genome_ids, query_dict, output_file, output_dir, genome_data, s
         # calculate gene_conservation
         gene_numerator = 0
         gene_denominator = 0
-        if pathway_id not in unique_pathway_features:
-            import pdb
-            pdb.set_trace()
-        for gene in unique_pathway_features[pathway_id]:
-            gene_numerator += len(unique_pathway_features[pathway_id][gene])
-            gene_denominator += len(pathway_genomes_found)
+        if pathway_id in unique_pathway_features:
+            for gene in unique_pathway_features[pathway_id]:
+                gene_numerator += len(unique_pathway_features[pathway_id][gene])
+                gene_denominator += len(pathway_genomes_found)
         if gene_denominator == 0:
             gene_conservation = 0
         else:
