5d44742b2427005110e77f54eff39a9f7ad15273
Clark Cucinell <ac.cucinell@holly.mcs.anl.gov>
2023-12-06 14:33:01 -0600

    still issue

--- lib/compare_systems_lib.py
+++ lib/compare_systems_lib.py
@@ -300,6 +300,9 @@ def run_families(genome_ids, query_dict, output_file, output_dir, genome_data, g
             output_json['genome_data'][field_key].append(value)
     output_json['genome_data']['genome_group'] = out_genome_groups
 
+    import pdb
+    pdb.set_trace()
+
     output_json_file = os.path.join(output_dir,output_file+'_proteinfams_tables.json')
     with open(output_json_file,"w") as o:
         o.write(json.dumps(output_json))
