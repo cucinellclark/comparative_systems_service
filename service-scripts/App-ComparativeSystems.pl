6d0733cc4ae763c17644773fa391c107d26689c4
Clark Cucinell <ac.cucinell@holly.mcs.anl.gov>
2024-01-04 14:00:42 -0600

    wtf

--- service-scripts/App-ComparativeSystems.pl
+++ service-scripts/App-ComparativeSystems.pl
@@ -184,16 +184,14 @@ sub run_codon_tree {
     #print $tmp encode_json(\%phylo_fields);
     #close($tmp);
 
-    my $tmp = File::Temp->new( CLEANUP => 0);
     my $output_json = encode_json(\%phylo_fields);
-    #"$work_dir/file.json"
-    open(my $file, '>', $tmp) or die "Couldn't open $tmp: $!";
+    open(my $file, '>', "$work_dir/file.json") or die "Couldn't open $file: $!";
     print $file $output_json;
     close($file);
 
     my $codon_app = "CodonTree";
     my $app_spec = find_app_spec($codon_app);
-    my @phylo_cmd = ("App-CodonTree","https://p3.theseed.org/services/app_service",$app_spec,$tmp); 
+    my @phylo_cmd = ("App-CodonTree","https://p3.theseed.org/services/app_service",$app_spec,$file); 
     #push(@phylo_cmd,"https://p3.theseed.org/services/app_service");
     #push(@phylo_cmd,$app_spec,"$work_dir/file.json");
 
