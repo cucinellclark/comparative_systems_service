58d6ecf404d3ae5ea0dfd1013d3c52dd28cc8319
Clark Cucinell <ac.cucinell@holly.mcs.anl.gov>
2024-01-04 13:52:38 -0600

    testing

--- service-scripts/App-ComparativeSystems.pl
+++ service-scripts/App-ComparativeSystems.pl
@@ -193,6 +193,7 @@ sub run_codon_tree {
     my $codon_app = "CodonTree";
     my $app_spec = find_app_spec($codon_app);
     my @phylo_cmd = ("App-CodonTree","https://p3.theseed.org/services/app_service",$app_spec,$file); 
+    print "here\n";
     print Dumper($file) . "\n";
     #push(@phylo_cmd,"https://p3.theseed.org/services/app_service");
     #push(@phylo_cmd,$app_spec,"$work_dir/file.json");
